328e21b10397075043a9fa9eb156cb1c1c07ee41
Jameson Mays <Jamesonmays@gmail.com>
2016-11-02 14:26:51 -0600

    Initial Commit with files

---
+++ app/src/main/java/venture/cs414/android/venturemonopoly/backEnd/Colors.java
@@ -0,0 +1,5 @@
+package venture.cs414.android.venturemonopoly.backEnd;
+
+public enum Colors {
+	RED, BLUE, BROWN, SKY, PURPLE, ORANGE, GREEN, YELLOW
+}
---
+++ app/src/main/java/venture/cs414/android/venturemonopoly/backEnd/SpaceType.java
@@ -0,0 +1,6 @@
+package venture.cs414.android.venturemonopoly.backEnd;
+
+public enum SpaceType {
+	PROPERTY, CHANCE, COMMUNITYCHEST, FREEPARKING,
+	INJAIL, JUSTVISITING, GO, LUXURYTAX, INCOMETAX, GOTOJAIL
+}
